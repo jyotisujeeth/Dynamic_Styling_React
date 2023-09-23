0e0b3983a52ab58a9f703877dca097e576dd7a91
jyotisujeeth <105577483+jyotisujeeth@users.noreply.github.com>
2023-09-24 05:13:10 +0530

    edited style components

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -1,17 +1,13 @@
+import styled from "styled-components";
 
-import styled from 'styled-components';
-import './Button.css';
-
-const Button = styled.Button`
-  
-    font: inherit;
-    padding: 0.5rem 1.5rem;
-    border: 1px solid #8b005d;
-    color: white;
-    background: #8b005d;
-    box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
-    cursor: pointer;
-  
+const Button = styled.button`
+  font: inherit;
+  padding: 0.5rem 1.5rem;
+  border: 1px solid #8b005d;
+  color: white;
+  background: #8b005d;
+  box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
+  cursor: pointer;
 
   &:focus {
     outline: none;
@@ -24,5 +20,13 @@ const Button = styled.Button`
     box-shadow: 0 0 8px rgba(0, 0, 0, 0.26);
   }
 `;
+
+// const Button = props => {
+//   return (
+//     <button type={props.type} className="button" onClick={props.onClick}>
+//       {props.children}
+//     </button>
+//   );
+// };
 
 export default Button;
